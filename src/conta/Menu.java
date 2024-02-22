f419fa095e5a3e048b02cff906e85306ba22e7dd
Joao-VPS <joaovictorpuglessabino@gmail.com>
2024-02-22 13:50:48 -0300

    Implementado função de alterar cores do layout ao digitar /color.

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -8,6 +8,7 @@ import conta.util.Cores;
 public class Menu {
 	static Scanner input = new Scanner(System.in);
 	static Conta conta1 = new ContaCorrente(123, 12345, "User", 10000f, 100);
+	static String color = Cores.TEXT_WHITE;
 	static boolean loop = true;
 	
 	public static void main(String[] args) {
@@ -85,45 +86,139 @@ public class Menu {
 		case "9": // Sair do aplicativo
 			System.out.println("Obrigado por usar nossos serviços! Volte sempre!");
 			loop = false;
+			
+			sobre();
 			System.exit(0);
 			break;
 
 		case "0": // Ver dados da conta
 			conta1.visualizar();
-			System.out.println("Aperte [ENTER] para continuar.");
 			break;
 
 		case "/color":
-			System.out.println("Ainda não implementado totalmente... Aguarde pelas versões futuras!");
-			break;
-			/*System.out.println("Parabéns! Você descobriu como alterar seu layout!");
-			System.out.print("Digite um número de 0 a 9: ");
-			System.out.println();
+			System.out.println("Parabéns! Você descobriu como alterar seu layout!");
 			
-			System.out.println("Alterando cor do layout para");
-			break;*/
+			while (true) {
+				System.out.print("Digite um número de 0 a 9 ou letra de A a F: ");
+				System.out.println();
+				if (setColor(input.nextLine())) break;
+			}
+			
+			break;
 		default:
 			System.err.println("Opção inválida, por favor, selecione outro valor!");
 		}
 	}
 	
 	public static void printMenu() {
-		System.out.println("╔════════════════════════════════════════════════╗");
-		System.out.println("║                 BANCO DA PRAÇA                 ║");
-		System.out.println("╠════════════════════════════════════════════════╣");
-		System.out.println("║ Selecione uma das opções abaixo para continuar ║");
-		System.out.println("║                                                ║");
-		System.out.println("║1 - Criar conta                                 ║");
-		System.out.println("║2 - Listar todas as contas                      ║");
-		System.out.println("║3 - Buscar conta por número                     ║");
-		System.out.println("║4 - Atualizar dados da conta                    ║");
-		System.out.println("║5 - Apagar conta                                ║");
-		System.out.println("║6 - Sacar                                       ║");
-		System.out.println("║7 - Depositar                                   ║");
-		System.out.println("║8 - Transferir valores entre contas             ║");
-		System.out.println("║9 - Sair                                        ║");
-		System.out.println("║0 - Ver dados da conta                          ║");
-		System.out.println("╚════════════════════════════════════════════════╝");
+		System.out.println(color + "╔════════════════════════════════════════════════╗");
+		System.out.println(color + "║                 BANCO DA PRAÇA                 ║");
+		System.out.println(color + "╠════════════════════════════════════════════════╣");
+		System.out.println(color + "║ Selecione uma das opções abaixo para continuar ║");
+		System.out.println(color + "║                                                ║");
+		System.out.println(color + "║1 - Criar conta                                 ║");
+		System.out.println(color + "║2 - Listar todas as contas                      ║");
+		System.out.println(color + "║3 - Buscar conta por número                     ║");
+		System.out.println(color + "║4 - Atualizar dados da conta                    ║");
+		System.out.println(color + "║5 - Apagar conta                                ║");
+		System.out.println(color + "║6 - Sacar                                       ║");
+		System.out.println(color + "║7 - Depositar                                   ║");
+		System.out.println(color + "║8 - Transferir valores entre contas             ║");
+		System.out.println(color + "║9 - Sair                                        ║");
+		System.out.println(color + "║0 - Ver dados da conta                          ║");
+		System.out.println(color + "╚════════════════════════════════════════════════╝");
 		System.out.print("Digite uma opção: ");
+	}
+	
+	public static boolean setColor(String value) {
+		switch (value) {
+		case "0":
+			color = Cores.TEXT_BLACK;
+			System.out.println(color + "Alterando cor do layout para PRETO");
+			return true;
+		case "1":
+			color = Cores.TEXT_BLUE;
+			System.out.println(color + "Alterando cor do layout para AZUL");
+			return true;
+		case "2":
+			color = Cores.TEXT_GREEN;
+			System.out.println(color + "Alterando cor do layout para VERDE");
+			return true;
+		case "3":
+			color = Cores.TEXT_CYAN;
+			System.out.println(color + "Alterando cor do layout para CIANO");
+			return true;
+		case "4":
+			color = Cores.TEXT_RED;
+			System.out.println(color + "Alterando cor do layout para VERMELHO");
+			return true;
+		case "5":
+			color = Cores.TEXT_PURPLE;
+			System.out.println(color + "Alterando cor do layout para ROXO");
+			return true;
+		case "6":
+			color = Cores.TEXT_YELLOW;
+			System.out.println(color + "Alterando cor do layout para DOURADO");
+			return true;
+		case "7":
+			color = Cores.TEXT_WHITE;
+			System.out.println(color + "Alterando cor do layout para CINZA CLARO");
+			return true;
+		case "8":
+			color = Cores.TEXT_BLACK_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para CINZA");
+			return true;
+		case "9":
+			color = Cores.TEXT_BLUE_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para AZUL CLARO");
+			return true;
+		case "a":
+			color = Cores.TEXT_GREEN_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para VERDE CLARO");
+			return true;
+		case "b":
+			color = Cores.TEXT_CYAN_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para CIANO CLARO");
+			return true;
+		case "c":
+			color = Cores.TEXT_RED_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para VERMELHO CLARO");
+			return true;
+		case "d":
+			color = Cores.TEXT_PURPLE_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para ROSA");
+			return true;
+		case "e":
+			color = Cores.TEXT_YELLOW_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para AMARELO");
+			return true;
+		case "f":
+			color = Cores.TEXT_WHITE_BRIGHT;
+			System.out.println(color + "Alterando cor do layout para BRANCO");
+			return true;
+		case "l":
+			color = Cores.TEXT_WHITE_BRIGHT;
+			System.out.println(color + "Adicionando efeito NEGRITO");
+			return true;
+		case "n":
+			color = Cores.TEXT_WHITE_BRIGHT;
+			System.out.println(color + "Adicionando efeito SUBLINHADO");
+			return true;
+		case "r":
+			color = Cores.TEXT_WHITE_BRIGHT;
+			System.out.println(color + "Restaurando todas a cores para o padrão de fábrica");
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	public static void sobre() {
+		System.out.println(Cores.TEXT_YELLOW_BRIGHT + "╔════════════════════════════════════════════════╗");
+		System.out.println(Cores.TEXT_YELLOW_BRIGHT + "║   Projeto desenvolvido por João V. P. Sabino   ║");
+		System.out.println(Cores.TEXT_YELLOW_BRIGHT + "║------------------------------------------------║");
+		System.out.println(Cores.TEXT_YELLOW_BRIGHT + "║E-Mail: joaovictorpuglessabino@gmail.com        ║");
+		System.out.println(Cores.TEXT_YELLOW_BRIGHT + "║GitHub: github.com/Joao-VPS                     ║");
+		System.out.println(Cores.TEXT_YELLOW_BRIGHT + "╚════════════════════════════════════════════════╝");
 	}
 }
